8aabf34b94b294066ab0e529b4f25860c13192a2
Inkwon Kim <inkweon7269@gmail.com>
2024-04-08 01:21:49 +0900

    AWS CloudFrontFullAccess 권한 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 const App = () => {
     return (
         <div>
-          <h1>Nice to meet you</h1>
+            <h1>Nice to meet you</h1>
+            <p>AWS에서 CloudFront 권한 추가</p>
         </div>
     );
 };
